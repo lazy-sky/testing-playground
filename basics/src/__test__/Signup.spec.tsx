73f041cb60191963411904d9de5986ec0226fe87
lazy-sky <1cielo@naver.com>
2024-04-02 22:00:33 +0900

    실패 케이스 작성 및 콘솔 테스트

--- basics/src/__test__/Signup.spec.tsx
+++ basics/src/__test__/Signup.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import SignupPage from "../pages/SignupPage";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,7 +9,7 @@ const queryClient = new QueryClient({
 });
 
 describe('회원가입 테스트', () => {
-  test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다.', () => {
+  test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다.', async () => {
     // given - 회원가입 페이지가 그려짐
     const routes = [
       {
@@ -22,12 +22,25 @@ describe('회원가입 테스트', () => {
       initialEntries: ["/signup"],
       initialIndex: 0
     })
+
     render(
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
-    // when
-    // then
+
+    // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
+
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'wrongPassword' } });
+
+    // then - 에러 메세지가 표시됨
+    const errorMessage = await screen.findByTestId("error-message");
+    // cypress랑 충돌 나서 잠시 지움
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).toHaveTextContent('비밀번호가 일치하지 않습니다');
   })
 }) 
