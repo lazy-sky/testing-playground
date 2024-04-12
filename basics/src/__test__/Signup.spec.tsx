7916d4f3ea27fde030d6a7771ceaf5f887574aad
lazy-sky <1cielo@naver.com>
2024-04-12 18:09:30 +0900

    성공 케이스와 beforeEach

--- basics/src/__test__/Signup.spec.tsx
+++ basics/src/__test__/Signup.spec.tsx
@@ -9,6 +9,26 @@ const queryClient = new QueryClient({
 });
 
 describe('회원가입 테스트', () => {
+  beforeEach(() => {
+    const routes = [
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      }
+    ]
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup"],
+      initialIndex: 0
+    })
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+  });
+
   test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다.', async () => {
     // given - 회원가입 페이지가 그려짐
     const routes = [
@@ -42,5 +62,24 @@ describe('회원가입 테스트', () => {
     // cypress랑 충돌 나서 잠시 지움
     expect(errorMessage).toBeInTheDocument();
     expect(errorMessage).toHaveTextContent('비밀번호가 일치하지 않습니다');
+  })
+
+  test('이메일을 입력하고, 비밀번호와 비밀번호 확인값이 일치하면 회원가입 버튼이 활성화된다.', () => {
+    // given - 회원가입 페이지가 그려짐
+    const signupButton = screen.getByRole('button', { name: '회원가입' });
+    expect(signupButton).toBeDisabled();
+
+    // when - 이메일 입력, 비밀번호, 비밀번호 확인값이 일치
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
+
+    fireEvent.change(emailInput, { target: { value: 'button-activated@email.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'password' } });
+
+
+    // then - 회원가입 버튼이 활성화
+    expect(signupButton).toBeEnabled();
   })
 }) 
